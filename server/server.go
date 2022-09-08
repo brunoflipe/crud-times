f8f3f5d555485905f687c28c67bb1d42e4c46332
brunoflipe <brunoflipe504@gmail.com>
2022-09-08 15:44:15 -0300

    first commmit

--- server/server.go
+++ server/server.go
@@ -16,7 +16,15 @@ func CreateServer() {
 	{
 		n1.GET("/listar", listarTimes)
 		n1.POST("/criar", criarTimes)
-
+		n1.PUT("/atualizar", atualizarTimes)
+		n1.DELETE("/deletar", deleteTimes)
+	}
+	n2 := r.Group("/tecnico")
+	{
+		n2.GET("/listar", listarTecnicos)
+		n2.POST("/criar", criarTecnicos)
+		n2.PUT("/atualizar", atualizarTecnicos)
+		n2.DELETE("/deletar", deleteTecnicos)
 	}
 
 	fmt.Println("API escutando na porta 2525")
@@ -35,5 +43,36 @@ func listarTimes(ctx *gin.Context) {
 func criarTimes(ctx *gin.Context) {
 	ctx.JSON(200, gin.H{
 		"mensagem": "rota para criar times",
+	})
+}
+func atualizarTimes(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"mensagem": "rota para atualizar times",
+	})
+}
+func deleteTimes(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"mensagem": "rota para deletar times",
+	})
+}
+func listarTecnicos(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"mensagem": "rota para listar tecnico",
+	})
+}
+
+func criarTecnicos(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"mensagem": "rota para criar tecnico",
+	})
+}
+func atualizarTecnicos(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"mensagem": "rota para atualizar tecnico",
+	})
+}
+func deleteTecnicos(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"mensagem": "rota para deletar tecnico",
 	})
 }
